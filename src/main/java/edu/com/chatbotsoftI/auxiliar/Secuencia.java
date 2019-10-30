590e98bb2a92daf16fbcc7c3a3b2bee588be6591
Diego La Faye <lafayediego@gmail.com>
2019-10-29 20:36:16 -0400

    modificada clase secuencia con posibles variables que llevara

--- src/main/java/edu/com/chatbotsoftI/auxiliar/Secuencia.java
+++ src/main/java/edu/com/chatbotsoftI/auxiliar/Secuencia.java
@@ -1,5 +1,30 @@
 package edu.com.chatbotsoftI.auxiliar;
 
 public class Secuencia {
-    
+    private int numero;
+    private String fase;
+
+    public Secuencia() {
+        
+    }
+
+    public Secuencia(String fase) {
+        this.fase = fase;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getFase() {
+        return fase;
+    }
+
+    public void setFase(String fase) {
+        this.fase = fase;
+    }
 }
