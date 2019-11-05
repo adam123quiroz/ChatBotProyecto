581ae642e4222128077bde81a86865a192c32e50
Adam <adam123quiroz@gmail.com>
2019-11-05 11:37:55 -0400

    Probando uso de la clase SendInvoice

--- src/main/java/edu/com/chatbotsoftI/bot/BoltonBot.java
+++ src/main/java/edu/com/chatbotsoftI/bot/BoltonBot.java
@@ -1,12 +1,12 @@
 package edu.com.chatbotsoftI.bot;
 
+import edu.com.chatbotsoftI.bl.BotBl;
 import edu.com.chatbotsoftI.bl.EventBl;
 import edu.com.chatbotsoftI.bl.UserBl;
-import edu.com.chatbotsoftI.bot.special.keyboard.KeyboardBot;
+import edu.com.chatbotsoftI.bot.special.keyboard.KbOptionsBot;
 import edu.com.chatbotsoftI.dto.EventDto;
-import edu.com.chatbotsoftI.dto.TypeEvent;
+import edu.com.chatbotsoftI.enums.TypeEvent;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendInvoice;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -29,12 +29,10 @@ public class BoltonBot extends TelegramLongPollingBot {
 
     static final String PROVIDER_TOKEN = "284685063:TEST:ZjhmMDU1MTM2MjVi";
 
-    private UserBl userBl;
-    private EventBl eventBl;
+    private BotBl botBl;
 
-    public BoltonBot(UserBl userBl, EventBl eventBl) {
-        this.userBl = userBl;
-        this.eventBl = eventBl;
+    public BoltonBot(BotBl botBl) {
+        this.botBl = botBl;
     }
 
     @Override
@@ -56,14 +54,14 @@ public class BoltonBot extends TelegramLongPollingBot {
 
         SendMessage sendMessageGreeting = new SendMessage().setChatId(update.getMessage().getChatId());
         Integer idChat = Integer.parseInt(message.getChatId().toString());
-        SendInvoice inv;
+
         List<String> options;
-        KeyboardBot keyboardBot;
+        KbOptionsBot keyboardBot;
         List<EventDto> eventDtos;
 
         switch(message.getText()) {
             case "Hola":
-                sendMessageGreeting = new SendMessage().setChatId(update.getMessage().getChatId()).setText("" +
+                sendMessageGreeting.setText("" +
                         "Hola "+ message.getChat().getFirstName() +", soy Bolton, para ayudarte necesito que entres en " +
                         "sesión o te registres:");
 
@@ -71,16 +69,20 @@ public class BoltonBot extends TelegramLongPollingBot {
                 options.add(OP_ADD_USERS);
                 options.add(OP_CONTINUE);
                 options.add(OP_LOG_IN_ADM);
-                keyboardBot = new KeyboardBot(options);
+                keyboardBot = new KbOptionsBot(options);
                 sendMessageGreeting.setReplyMarkup(keyboardBot);
                 execute(sendMessageGreeting);
                 break;
+
             case OP_ADD_USERS:
-//                sendMessageGreeting = new SendMessage()
-//                        .setChatId(update.getMessage().getChatId())
-//                        .setText("Rellene los siguientes espacios \n" +
-//                                "Cual es tu nombre ?");
+/*
+                sendMessageGreeting = new SendMessage()
+                        .setChatId(update.getMessage().getChatId())
+                        .setText("Rellene los siguientes espacios \n" +
+                                "Cual es tu nombre ?");
+*/
                 break;
+
             case OP_CONTINUE:
                 sendMessageGreeting = new SendMessage()
                         .setChatId(update.getMessage().getChatId())
@@ -89,60 +91,26 @@ public class BoltonBot extends TelegramLongPollingBot {
                 options.add(OP_MUSIC);
                 options.add(OP_MOVIE);
                 options.add(OP_MUSEUM);
-                keyboardBot = new KeyboardBot(options);
+                keyboardBot = new KbOptionsBot(options);
                 sendMessageGreeting.setReplyMarkup(keyboardBot);
                 execute(sendMessageGreeting);
                 break;
+
             case OP_LOG_IN_ADM:
                 System.out.println("Admi ");
                 break;
-            case OP_MOVIE:
-                eventDtos = eventBl.findAllEventByTypeEvent(TypeEvent.MOVIE.getTypeEvent());
 
-                for (EventDto event:
-                        eventDtos) {
-                    String description = String.format("" +
-                            "Fecha: " + event.getDate() + "\n" +
-                            "Hora: " + event.getStarttime() + "\n"
-//                            "Categoria: " + event.getCategory() + "\n" +
-//                            "Direccion: " + event.getAddress()
-                    );
-
-                    inv = new SendInvoice(
-                            idChat,
-                            event.getNameevent(),
-                            description,
-                            "Visa", PROVIDER_TOKEN,
-                            "StartParam", "USD", Collections.singletonList(new LabeledPrice("label", 200)))
-                            .setPhotoUrl("https://www.yucatan.com.mx/wp-content/uploads/2019/03/2491246.jpg-r_1920_1080-f_jpg-q_x-xxyxx.jpg");
-
-                    execute(inv);
-                }
-                break;
-
-            case OP_MUSEUM:
-                eventDtos = eventBl.findAllEventByTypeEvent(TypeEvent.MUSEUM.getTypeEvent());
-
-                for (EventDto event:
-                        eventDtos) {
-                    String description = String.format("" +
-                                    "Fecha: " + event.getDate() + "\n" +
-                                    "Hora: " + event.getStarttime() + "\n"
-//                            "Categoria: " + event.getCategory() + "\n" +
-//                            "Direccion: " + event.getAddress()
-                    );
-
-                    inv = new SendInvoice(
-                            idChat,
-                            event.getNameevent(),
-                            description,
-                            "Visa", PROVIDER_TOKEN,
-                            "StartParam", "USD", Collections.singletonList(new LabeledPrice("label", 200)))
-                            .setPhotoUrl("https://www.travelreport.mx/wp-content/uploads/2018/08/los-mejores-museos-en-aguascalientes-portada.jpg");
-
-                    execute(inv);
-                }
-                break;
+//            case OP_MOVIE:
+//                eventDtos = eventBl.findAllEventByTypeEvent(TypeEvent.MOVIE.getTypeEvent());
+//                showEventsInformation(eventDtos, idChat,
+//                        "https://www.yucatan.com.mx/wp-content/uploads/2019/03/2491246.jpg-r_1920_1080-f_jpg-q_x-xxyxx.jpg?width=1200&enable=upscale");
+//                break;
+//
+//            case OP_MUSEUM:
+//                eventDtos = eventBl.findAllEventByTypeEvent(TypeEvent.MUSEUM.getTypeEvent());
+//                showEventsInformation(eventDtos, idChat,
+//                        "https://ep00.epimg.net/elviajero/imagenes/2016/11/23/album/1479923555_950451_1479926380_album_normal.jpg");
+//                break;
 
 
             default:
@@ -150,6 +118,29 @@ public class BoltonBot extends TelegramLongPollingBot {
         }
     }
 
+    public void showEventsInformation(List<EventDto> eventDtos, int idChat, String url) throws TelegramApiException {
+//        SendInvoice inv;
+        SendInvoice inv;
+        for (EventDto event:
+                eventDtos) {
+            String description = String.format("" +
+                            "Fecha: " + event.getDate() + "\n" +
+                            "Hora: " + event.getStarttime() + "\n"
+//                            "Categoria: " + event.getCategory() + "\n" +
+//                            "Direccion: " + event.getAddress()
+            );
+            inv = (SendInvoice) new SendInvoice(
+                    idChat,
+                    event.getNameevent(),
+                    description,
+                    "Visa", PROVIDER_TOKEN,
+                    "StartParam", "USD", Collections.singletonList(new LabeledPrice("label", 200))
+            )
+                    .setPhotoUrl(url);
+
+            this.execute(inv);
+        }
+    }
 
     @Override
     public String getBotUsername() {
